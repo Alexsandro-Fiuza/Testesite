144ddd3b9256035a36ab0cb021c938ac34b8561c
alexx <alexxfiuza33@gmail.com>
2023-08-06 20:58:03 -0300

    update js and css

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function ToggleMode() {
-  const c = document.documentElement
-  c.classList.toggle("light")
-  const a = document.querySelector("#profile img")
-  c.classList.contains("light")
-    ? a.setAttribute("src", "./assets/avatar-lightzero.png")
-    : a.setAttribute("src", "./assets/zertwoavatar.png")
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-lightzero.png")
+  } else {
+    img.setAttribute("src", "./assets/zertwoavatar.png")
+  }
 }
