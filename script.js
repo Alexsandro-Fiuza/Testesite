959333c6a65d20cfeb5ed302143ff55bb1b231dd
alexx <alexxfiuza33@gmail.com>
2023-08-06 20:25:14 -0300

    Update Comprimindo Js and Css

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 function ToggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
-
-  const img = document.querySelector("#profile img")
-
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-lightzero.png")
-  } else {
-    img.setAttribute("src", "./assets/zertwoavatar.png")
-  }
+  const a = document.documentElement
+  a.classList.toggle("light")
+  const b = document.querySelector("#profile img")
+  a.classList.contains("light")
+    ? b.setAttribute("src", "./assets/avatar-lightzero.png")
+    : b.setAttribute("src", "./assets/zertwoavatar.png")
 }
